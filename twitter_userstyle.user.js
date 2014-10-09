ffdf57cb207c4d188f1aba91a5130721e46806a9
scturtle <scturtle@gmail.com>
2014-10-09 12:37:42 +0800

    quick fix

--- twitter_userstyle.user.js
+++ twitter_userstyle.user.js
@@ -2,9 +2,14 @@
 // @name        twitter-userstyle
 // @namespace   https://github.com/mdo/twitter-userstyle
 // @include     https://twitter.com/*
-// @resource    CSS https://rawgithub.com/scturtle/twitter-userstyle/master/twitter.css
+// @resource    CSS https://raw.githubusercontent.com/scturtle/twitter-userstyle/master/twitter.css
 // @grant       GM_addStyle
 // @grant       GM_getResourceText
 // ==/UserScript==
 
 GM_addStyle(GM_getResourceText("CSS"));
+/* quick fix search box board */
+document.getElementById("search-query").style.cssText="border: 0px";
+/* fix new profile icon top right */
+document.getElementById("user-dropdown-toggle").style.cssText="height: 32px !important";
+document.getElementById("user-dropdown").parentElement.style.cssText="margin-right: 10px !important";
